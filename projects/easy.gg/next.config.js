bc4cc4595503c6e8aeab644e3a0693b6610814d1
Mogen Cheng <mogen10@live.ca>
2022-02-07 01:11:39 -0500

    update webpack to default values

--- projects/easy.gg/next.config.js
+++ projects/easy.gg/next.config.js
@@ -17,10 +17,6 @@ const nextConfig = {
       },
     ]
   },
-  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-    // Important: return the modified config
-    return config
-  },
 }
 
 module.exports = nextConfig
