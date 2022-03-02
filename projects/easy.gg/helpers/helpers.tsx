8f1286591c94360889ddedd1b8cb0a237fecce01
Mogen Cheng <mogen10@live.ca>
2022-03-02 18:33:25 -0500

    fix type

--- projects/easy.gg/helpers/helpers.tsx
+++ projects/easy.gg/helpers/helpers.tsx
@@ -29,6 +29,6 @@ export const userProfilePictureThumbnailProxy = (url: string) => {
 export const twitchClipProxy = (url: string) => {
     const urlLink = new URL(`${window.location.protocol}//${window.location.host}/api/twitch/videoProxy`);
     urlLink.searchParams.append('url', url);
-    return urlLink;
+    return urlLink.href;
     // return `/proxy/twitchClip/${url.replace('https://production.assets.clips.twitchcdn.net/', '')}`;
 }
